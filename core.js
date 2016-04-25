f7f8df88eab8ee0b14e2acb380f80db902314f55
Mike <kenr.mk@gmail.com>
2016-04-25 21:17:21 +0200

    dalsi komentare

--- core.js
+++ core.js
@@ -1,5 +1,9 @@
+/// Páteřní ústředna událostí
+var events;
 if (typeof global == 'object') {
-	module.exports = new (require('events').EventEmitter);
+	// instance nativniho EventEmitteru pri behu v prostredi Node.js a export jako Node modul
+	module.exports = events = new (require('events').EventEmitter);
 } else {
+	// instanciace vlastni implementace EventEmitteru pri behu v prostredi prohlizece do globalni promenne
 	var events = new EventEmitter;
 }
--- ui.js
+++ ui.js
@@ -1,14 +1,21 @@
+/// DOM element <flexus-toolbar>
 var $toolbar = document.querySelector('flexus-toolbar')
+/// DOM element #display s vystupem kalkulacky
 var $display = document.querySelector('#display');
+/// DOM element #buttons obsahujici ridici tlacitka kalkulacky
 var $buttons = document.querySelector('#buttons');
+/// DOM element tlacitko backspace
 var $backspace = document.querySelector('#buttons button[data-action="backspace"]');
 
+/// sirka toolbaru
 var toolbarWidth = $toolbar.offsetWidth;
+/// vyska toolbaru
 var toolbarHeight = $toolbar.offsetHeight;
 
-
 // TODO recalculate on window resize
+/// instance Ripple tridy pro ripple efekt kliknuti
 var displayRipple = new Ripple($toolbar, {x: toolbarWidth, y: toolbarHeight});
+/// instance Ripple tridy pro ripple efekt chyby
 var errorRipple = new Ripple($toolbar, {
 	x: toolbarWidth,
 	y: toolbarHeight,
@@ -20,6 +27,7 @@ var errorRipple = new Ripple($toolbar, {
 		$toolbar.className = 'error';
 	}
 })
+/// instance Ripple tridy pro ripple efekt odstraneni chyby
 var unerrorRipple = new Ripple($toolbar, {
 	x: toolbarWidth,
 	y: toolbarHeight,
@@ -33,24 +41,29 @@ var unerrorRipple = new Ripple($toolbar, {
 });
 
 
+/// event listener naslouchajici na kliknuti a 
 $buttons.addEventListener('click', e => {
+	/// vypise znak pri stisku tlacitka s vyznamovym znakem nebo znamenkem
 	if (e.target.dataset.char) {
 		events.emit('display-char', e.target.dataset.char);
 	}
+	/// pri stisku tlacitka rovnase spocita a vypise vysledek
 	if (e.target.dataset.action == 'equals') {
 		events.emit('display-result');
 	}
 })
 
-// key press listeners
+/// event listener naslouchajici na stisk klavesnice
 window.addEventListener('keydown', e => {
 	if (e.keyCode !== 82 && !e.shiftKey) {
-		// prevent default behavior except for ctrl+r for dev purposes
+		/// prevent default behavior except for ctrl+r for dev purposes
 		e.preventDefault();
 	}
 	var code = e.keyCode;
 	if (code >= 48 && code <= 57) {
+		/// stisk numericke klavesy
 		events.emit('display-char', code - 48)
+		/// stisk anglicke numericke klavesy
 	} else if (code >= 96 && code <= 105) {
 		events.emit('display-char', code - 96)
 	} else switch (code) {
@@ -67,30 +80,41 @@ window.addEventListener('keydown', e => {
 	}
 })
 
+/// naveseni event listeneru na dotyk nebo stisknuti tlacitka backspace
 if ('ontouchstart' in window) $backspace.addEventListener('touchstart', backspaceStart)
 if ('onmousedown' in window) $backspace.addEventListener('mousedown', backspaceStart)
 if ('onpointerdown' in window) $backspace.addEventListener('pointerdown', backspaceStart)
-
 if ('ontouchend' in window) $backspace.addEventListener('touchend', backspaceEnd)
 if ('onmouseup' in window) $backspace.addEventListener('mouseup', backspaceEnd)
 if ('onpointerup' in window) $backspace.addEventListener('pointerup', backspaceEnd)
-
+/// promenna s informaci o bezitim timeout
 var backspaceTimeout;
+/** 
+* @brief callback vyprseni timeout, ktery vymaze vypis kalkulacky
+*/
 function onBackspaceTimeout() {
 	events.emit('display-clear');
 }
+/** 
+* @brief callback stisku klavesy backspace. nastavi pul sekundovy timeout pro uplne vymazani displeje
+*/
 function backspaceStart(e) {
 	backspaceTimeout = setTimeout(onBackspaceTimeout, 500)
 }
+/** 
+* @brief callback opusteni klavesy backspace. zabrani timeoutu a smaze pouze posledni znak na vypisu
+*/
 function backspaceEnd(e) {
 	clearTimeout(backspaceTimeout);
 	events.emit('display-backspace');
 }
 
-
+/// informaci je-li vypsat chybovy stav
 var isError = false;
+/// pocitany matematicky vyraz
 var expression = '';
 
+/// pri udalosti math-error se vypise chyba, zmeni vzhled ui a zobrazi chybovy ripple
 events.on('math-error', (message = 'error') => {
 	if (isError) return;
 	isError = true;
@@ -99,6 +123,7 @@ events.on('math-error', (message = 'error') => {
 	expression = '';
 	$display.textContent = message;
 })
+/// pri udalosti math-unerror se navrati vzhled ui a skryje chybovy ripple
 events.on('math-unerror', (message = '') => {
 	if (!isError) return;
 	isError = false
@@ -109,6 +134,7 @@ events.on('math-unerror', (message = '') => {
 	}
 })
 
+/// pri udalosti display-char se vypise zadany znak na vystup
 events.on('display-char', (char = '') => {
 	if (isError) {
 		events.emit('math-unerror');
@@ -116,6 +142,7 @@ events.on('display-char', (char = '') => {
 	expression += char;
 	$display.textContent = expression;
 })
+/// pri udalosti display-backspace se vymaze posledni znak z vystupu
 events.on('display-backspace', () => {
 	if (isError) {
 		events.emit('math-unerror');
@@ -125,6 +152,7 @@ events.on('display-backspace', () => {
 		$display.textContent = expression;
 	}
 })
+/// pri udalosti display-clear se smaze veskery vypis
 events.on('display-clear', () => {
 	if (isError) {
 		events.emit('math-unerror');
@@ -133,6 +161,7 @@ events.on('display-clear', () => {
 	}
 	$display.textContent = expression = '';
 })
+/// pri udalosti display-result se spocita aktualni vyraz a vypise se (nebo pripadna chyba) na vystup
 events.on('display-result', () => {
 	displayRipple.replay();
 	if (expression.length) {
